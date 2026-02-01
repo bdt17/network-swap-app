44630749163b2b1d2ab99683b320e0b941d1c5d3
bdt17 <brett.thomas29.97@gmail.com>
2026-02-01 16:58:42 -0700

    Fix routes: single root devices#index

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,12 +1,24 @@
 class DevicesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
-    if user_signed_in?
-      @devices = Device.all
-    else
-      redirect_to new_user_session_path
-    end
+    @devices = Device.all
   end
 end
+
+
+
+
+
+#class DevicesController < ApplicationController
+ # def index
+  #  if user_signed_in?
+   #   @devices = Device.all
+    #else
+     # redirect_to new_user_session_path
+   # end
+  #end
+#end
 
 
 #class DevicesController < ApplicationController
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # Thomas IT Network Swap App
   resources :devices
-  resources :sites  
+  resources :sites
   resources :swaps
-  root 'devices#index'
+  root 'devices#index'  # ← SINGLE root - NOW ACTIVE
   
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   # API (JSON)
   namespace :api, defaults: { format: :json } do
     resources :devices, only: [:index, :show]
