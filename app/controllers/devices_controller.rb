0218855f71dbe6a10f6c208a0c439c59b7f9b384
bdt17 <brett.thomas29.97@gmail.com>
2026-02-01 16:26:21 -0700

    Fix 500: authenticate_user! on DevicesController

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,8 +1,19 @@
 class DevicesController < ApplicationController
-  before_action :authenticate_user!
-  
   def index
-#    @devices = Device.includes(:site).order(warranty_expires: :asc, created_at: :desc)
-   @devices = Device.all  # Simple query 
- end
+    if user_signed_in?
+      @devices = Device.all
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
+
+
+#class DevicesController < ApplicationController
+ # before_action :authenticate_user!
+  
+ # def index
+#    @devices = Device.includes(:site).order(warranty_expires: :asc, created_at: :desc)
+  # @devices = Device.all  # Simple query 
+ #end
+#end
