f87a2c3f97e52d303d2a1c1e99effb7c28cf3f18
bdt17 <brett.thomas29.97@gmail.com>
2026-02-01 15:41:50 -0700

    Fix 500: SECRET_KEY_BASE + DevicesController auth

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,6 +2,7 @@ class DevicesController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @devices = Device.includes(:site).order(warranty_expires: :asc, created_at: :desc)
-  end
+#    @devices = Device.includes(:site).order(warranty_expires: :asc, created_at: :desc)
+   @devices = Device.all  # Simple query 
+ end
 end
