d14127641683e8eb3b7cbff6490c556ad34e7433
bdt17 <brett.thomas29.97@gmail.com>
2026-02-01 17:28:43 -0700

    NUCLEAR: Pure Rack 302 redirect - bypass ALL crashes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get "home/index"
-  # TEMPORARY - Nuclear option
-  get "/", to: "home#index"
+  # NUCLEAR: Pure Rack response - bypasses ALL Rails/Devise/middleware
+  get "/", to: proc { [302, {'Location' => '/users/sign_in'}, []] }
   
   devise_for :users
   resources :devices
-  resources :sites
+  resources :sites  
   resources :swaps
   
   get "up" => "rails/health#show", as: :rails_health_check
-  namespace :api, defaults: { format: :json } do
-    resources :devices, only: [:index, :show]
-  end
 end
