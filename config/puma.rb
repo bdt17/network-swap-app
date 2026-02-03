ebe388b05de67fe21d269b4af097e2aa6396b905
bdt17 <brett.thomas29.97@gmail.com>
2026-02-02 19:49:59 -0700

    Puma config + Sinatra rackup

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,12 @@
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-workers     ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+threads_count = Integer(ENV["MAX_THREADS"] || 5)
+threads threads_count, threads_count
 
 preload_app!
-plugin :tmp_restart
+
+port ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RACK_ENV") { "development" }
+
+stdout_redirect "/rails/log/puma.stdout.log", "/rails/log/puma.stderr.log", true
+
+rackup DefaultRackup
