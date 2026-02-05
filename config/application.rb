a2d0906229577bbc5dbeb953a94bce33dddfe4bf
bdt17 <brett.thomas29.97@gmail.com>
2026-02-04 21:25:48 -0700

    DISABLE ActiveStorage + inline secret_key_base

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,18 @@ require_relative "boot"
 
 require "rails/all"
 
+# DISABLE ActiveStorage
 Bundler.require(*Rails.groups)
-
 module NetworkSwapApp
   class Application < Rails::Application
     config.load_defaults 8.1
-    config.api_only = false
-    config.generators.system_tests = nil
+    config.api_only = true
     
-    # Fix TailwindCSS assets
+    # BLOCK ActiveStorage initialization
+    config.active_storage.draw_routes = false
+    initializer "disable_activestorage", before: :load_config_initializers do |app|
+      app.config.active_storage.service = nil
+      ActiveStorage::Engine.skip = true
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,12 @@
 Rails.application.configure do
-  config.eager_load = false
   config.cache_classes = true
+  config.eager_load = true
+  config.api_only = true
+  
+  # Generate inline secret - no env vars needed
+  config.secret_key_base = "a"*64
+  
+  # Disable all storage/verifiers
+  config.active_storage.service_configurations = {}
+  config.active_storage.skip_file_verification = true
 end
