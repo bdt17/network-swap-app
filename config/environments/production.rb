b4a4e888bcf27f8990a6716787bed14464a6d1dc
bdt17 <brett.thomas29.97@gmail.com>
2026-02-05 13:01:22 -0700

    Complete production.rb with secure secret_key_base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = false
-  config.secret_key_base = "a1b2c3d4e5f6789012345678901234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef123456"
+  config.secret_key_base = "f8e7d6c5b4a3928175064f3e2d1c0b9a8f7e6d5c4b3a2918076f5e4d3c2b1a0987654321fedcba987654321fedcba9876543210fedcba987654321fedcba9876543210"
 end
