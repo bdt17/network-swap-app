dcefd77162d813f87254c54e5f43bda5ada0e94b
bdt17 <brett.thomas29.97@gmail.com>
2026-02-04 21:36:20 -0700

    MINIMAL: no ActiveStorage refs + inline secret

--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,5 @@ module NetworkSwapApp
   class Application < Rails::Application
     config.load_defaults 8.1
     config.api_only = true
-    
-    # Disable ActiveStorage routes
-    config.active_storage.draw_routes = false
-    
-    # Skip ActionText (also in your bundle)
-    config.action_text = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,11 +2,5 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
   config.api_only = true
-  
-  # Inline secret - no env vars needed
   config.secret_key_base = "a1b2c3d4e5f6789012345678901234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef123456"
-  
-  # Disable storage features
-  config.active_storage.service = :test
-  config.active_storage.variant_processor = :vips
 end
