5ca0f014db69aa61e70bf93bd5579979472d33b0
bdt17 <brett.thomas29.97@gmail.com>
2026-02-04 19:02:25 -0700

    fix enum syntax

--- test_scripts/app/models/swap_ticket.rb
+++ test_scripts/app/models/swap_ticket.rb
@@ -1,23 +1,9 @@
 class SwapTicket < ApplicationRecord
-  enum :status, {
+  enum status: {
     pending: 0,
     scheduled: 1,
     in_progress: 2,
     completed: 3,
     failed: 4
   }
-
-  # other associations/validations here
 end
-
-
-
-#class SwapTicket < ApplicationRecord
- # enum :status, { pending: 0, scheduled: 1, in_progress: 2, completed: 3, failed: 4 }
-#end
-
-
-#class SwapTicket < ApplicationRecord
- # enum :status, { pending: 0, scheduled: 1, in_progress: 2, completed: 3, failed: 4 }
-  # Add your associations/validations here
-#end
