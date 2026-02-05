bace8a7d50dc5c6bff002dc8a888a32ccb2d5147
bdt17 <brett.thomas29.97@gmail.com>
2026-02-04 21:41:17 -0700

    FIX: storage.yml + eager_load=false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
   config.cache_classes = true
-  config.eager_load = true
+  config.eager_load = false  # ← KEY FIX
   config.secret_key_base = "a1b2c3d4e5f6789012345678901234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef123456"
 end
