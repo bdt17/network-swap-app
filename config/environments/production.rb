321cefe8896a4dd14f5f11c1b1c8f683c4d2b779
bdt17 <brett.thomas29.97@gmail.com>
2026-02-05 15:48:53 -0700

    Fix Render deploy: Procfile + render.yaml + assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,3 +3,5 @@ Rails.application.configure do
   config.eager_load = false
   config.secret_key_base = "f8e7d6c5b4a3928175064f3e2d1c0b9a8f7e6d5c4b3a2918076f5e4d3c2b1a0987654321fedcba987654321fedcba9876543210fedcba987654321fedcba9876543210"
 end
+Rails.application.config.assets.compile = false
+Rails.application.config.public_file_server.enabled = true
