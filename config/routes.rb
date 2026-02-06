f56ffe947a883607862468d792f5d5ce728ca85a
bdt17 <brett.thomas29.97@gmail.com>
2026-02-05 22:23:28 -0700

    FINAL routes.rb fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "home#index"  # Serves public/index.html
+  root "home#index"
   get '/health', to: ->(_env) { [200, {'Content-Type' => 'text/plain'}, ['OK']] }
 end
