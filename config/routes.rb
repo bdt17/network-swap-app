ca4ccdd29cb6f4b2ed3d7ff23246865ae89454a7
bdt17 <brett.thomas29.97@gmail.com>
2026-02-05 19:11:16 -0700

    FIX: routes.rb namespace + HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,17 @@
 Rails.application.routes.draw do
   root "home#index"
   
-  # Health + Static
+  # Health + Static endpoints
   get "health", to: ->(env) { [200, {"Content-Type" => "application/json"}, ["OK"]] }
   get "inventory", to: ->(env) { [200, {"Content-Type" => "application/json"}, ["Inventory API LIVE"]] }
   
-  # API Namespace
+  # API routes (FIXED namespace)
   namespace :api do
     resources :devices do
       member do
-        get :ai_recommend, to: 'devices/ai_recommend#ai_recommend'
+        get :ai_recommend
       end
     end
     resources :audit_logs, only: [:index]
   end
-end
-
-namespace :api do
-  resources :devices, only: [:index]
-  resource :ai_recommend, only: [:show], path: 'devices/1/ai_recommend'
-  resources :audit_logs, only: [:index]
 end
