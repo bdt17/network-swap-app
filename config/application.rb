13c6ffbcd3a85224e7f4acfeeba68fd1637d04b2
bdt17 <brett.thomas29.97@gmail.com>
2026-02-07 18:47:57 -0700

    🔥 FIX: Complete Rails config for Render

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,22 @@
 require_relative "boot"
-require "rails/all"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "rails/test_unit/railtie"
+
 Bundler.require(*Rails.groups)
 
 module NetworkSwapApp
   class Application < Rails::Application
-    config.api_only = true
     config.load_defaults 8.1
+    config.api_only = true
     config.public_file_server.enabled = true
   end
 end
