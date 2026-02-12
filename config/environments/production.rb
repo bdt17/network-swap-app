6cc0e2beb16b38d03e8a02abf097ff0bbfdf9edc
bdt17 <brett.thomas29.97@gmail.com>
2026-02-12 12:27:50 -0700

    FIX: config.require_master_key to production.rb only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,4 +2,6 @@ Rails.application.configure do
   config.eager_load = true
   config.force_ssl = true
   config.log_level = :info
+  config.cache_classes = true
+  config.require_master_key = false
 end
