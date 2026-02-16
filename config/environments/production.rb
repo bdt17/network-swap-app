7c64b1cebd78caa9654a18f686dbd77c111dc1de
bdt17 <your-email@example.com>
2026-02-16 15:41:36 -0700

    Phase 14/10: production.rb + rails secret fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,15 @@
 Rails.application.configure do
+  # Disable credentials encryption (Render fix)
+  config.require_master_key = false
+  
+  # Core production settings
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
-  }
-  config.log_level = :warn
-  config.active_support.deprecation = :notify
+  config.log_level = :info
+  
+  # Phase 14/10 Thomas IT
+  config.force_ssl = true
 end
