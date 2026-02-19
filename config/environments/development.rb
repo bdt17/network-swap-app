31c9f186d52495d2d83bd2e9474bc53a3bff4033
bdt17 <your-email@example.com>
2026-02-18 20:13:57 -0700

    PHASE 14: Drone inspections API complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,1 +1,11 @@
-Rails.logger = ActiveSupport::Logger.new(STDOUT)
+Rails.application.configure do
+  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.log_level = :debug
+  config.log_formatter = ActiveSupport::Logger::SimpleFormatter.new
+
+  config.cache_classes = false
+  config.eager_load = false
+  config.consider_all_requests_local = true
+  config.server_timing = true
+  config.active_record.migration_error = :page_load
+end
